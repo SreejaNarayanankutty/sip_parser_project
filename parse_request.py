e22c3d37cfc2206d161c989a796393b5a32a9338
SreejaNarayanankutty <nsreeja2@gmail.com>
2024-10-03 19:29:49 +0200

    added changes to print details of sip message, fetch given header and verify compliance based on rfc 8.1.1

--- parse_request.py
+++ parse_request.py
@@ -1,7 +1,5 @@
 import re
 import argparse
-import string
-
 
 def parse_sip_file(file_path):
     with open(file_path, 'r') as sip_file:
@@ -38,7 +36,7 @@ def print_sip_message_details(file_path):
     body_data = ""
     method_name = re.split(r'\ssip:', sip_message_details.get('request_uri'))[0]
     for key in sip_headers.keys():
-        header_data = ''.join([header_data, f'\t{key}: {sip_headers[key]}\n'])
+        header_data = ''.join([header_data, rf'\t{key}: {sip_headers[key]}\n'])
     for data in sip_body:
         body_data = ''.join([body_data, data])
 
@@ -57,33 +55,64 @@ def verify_and_fetch_header_data(header_name_to_verify, file_path):
     for key in sip_headers_from_file.keys():
         if key.lower() == header_name_to_verify:
             header_presence_flag = True
-            header_value = ''.join([header_value, f'\t{key}: {sip_headers_from_file[key]}\n'])
+            header_value = ''.join([header_value, rf'\t{key}: {sip_headers_from_file[key]}\n'])
 
     if header_presence_flag:
         print(f'\"{header_name_to_verify}\" header is present in SIP request as below:\n{header_value}')
     else:
         print(f'\"{header_name_to_verify}\" header is not present in SIP Request.')
 
+
 def validate_sip_message(file_path):
     mandatory_headers = {'To', 'From', 'CSeq', 'Call-Id', 'Via', 'Max-Forwards'}
     sip_message_details = fetch_sip_message_details(file_path)
-    sip_headers = sip_message_details.get('headers').keys()
-    sip_headers_lower = [item.lower() for item in sip_headers]
-    missing_headers_list= []
+    sip_headers = sip_message_details.get('headers')
+    sip_headers_keys = sip_headers.keys()
+    request_uri = sip_message_details.get('request_uri')
+    method_name = sip_message_details.get('method')
+    sip_headers_lower = [item.lower() for item in sip_headers_keys]
+    missing_headers_list = []
     missing_headers = ''
-    validation_flag = False
+    validation_flag = True
     for i in mandatory_headers:
-        miss_flag = False
+        presence_flag = False
         for j in sip_headers_lower:
             if i.lower() == j.lower():
-                miss_flag = True
+                presence_flag = True
                 continue
-        if not miss_flag:
+        if not presence_flag:
             missing_headers_list.append(i)
             missing_headers = ','.join(missing_headers_list)
+
     if not missing_headers_list:
-        validation_flag = True
+        to_header_val = sip_headers.get('To')
+        from_header_val = sip_headers.get('From')
+        domain_name_in_to_header = re.findall(r'sip:\w+@\w+\.\w+', to_header_val)[0]
+        if request_uri and method_name=='REGISTER' and re.search(r'[A-Z]+\ssip:.+\sSIP/2.0', request_uri):
+            pass
+        elif request_uri and re.search(rf'[A-Z]+\s{re.escape(domain_name_in_to_header)}\sSIP/2.0', request_uri):
+            pass
+        else:
+            validation_flag = False
+            print(f'\"""\nThe verification of the request failed due to the following reason(s):\n'
+                  f'Error: The Request-URI is not valid, as required by "RFC3261 Section 8.1.1.1"\n'
+                  f'\""".')
+        if re.search(r'[a-zA-Z]*<(sip|sips|tel):.+>', to_header_val,  re.IGNORECASE):
+            pass
+        else:
+            validation_flag = False
+            print(f'\"""\nThe verification of the request failed due to the following reason(s):\n'
+                  f'Error: The To header is not valid, as required by "RFC3261 Section 8.1.1.1"\n'
+                  f'\""".')
+        if re.search(r'.*<(sip|sips|tel):.+>;tag=.+', from_header_val,  re.IGNORECASE):
+            pass
+        else:
+            validation_flag = False
+            print(f'\"""\nThe verification of the request failed due to the following reason(s):\n'
+                  f'Error: The From header is not valid, as required by "RFC3261 Section 8.1.1.1"\n'
+                  f'\""".')
     else:
+        validation_flag = False
         print(f'\"""\nThe verification of the request failed due to the following reason(s):\n'
               f'Error: The \"{missing_headers}\" header is missing, as required by "RFC3261 Section 8.1.1"\n'
               f'\""".')
@@ -96,24 +125,28 @@ def validate_sip_message(file_path):
 def main():
     # Defining command line option to parse sip test file and perform various function
     arg_parsor = argparse.ArgumentParser(description="SIP Request Parser Command Line Interface", argument_default=argparse.SUPPRESS)
+    # arg_parsor.add_argument('-p', action='store_true', help="flag p to print details in sip message")
+    # arg_parsor.add_argument('--print', action='store_true', help="flag print to print details in sip message")
 
-    arg_parsor.add_argument('-p', action='store_true', help="flag p to print details in sip message")
-    arg_parsor.add_argument('--print', action='store_true', help="flag print to print details in sip message")
+    # arg_parsor.add_argument('-e', action='store_true', help="flag p to verify given header details in sip message")
+    # arg_parsor.add_argument('--exists', action='store_true', help="flag print to verify given header details in sip message")
+    # arg_parsor.add_argument('header', type=str, help="header to search in sip message")
 
-    #arg_parsor.add_argument('-e', action='store_true', help="flag p to verify given header details in sip message")
-    #arg_parsor.add_argument('--exists', action='store_true', help="flag print to verify given header details in sip message")
-    #arg_parsor.add_argument('header', type=str, help="header to search in sip message")
+    arg_parsor.add_argument('-v', action='store_true', help="flag v to validate given sip message based on RFC3261")
+    arg_parsor.add_argument('--validate', action='store_true', help="flag validate to verify given sip message based on RFC3261")
 
     arg_parsor.add_argument('file', type=str, help="Path to the text file containing SIP request")
-
+    #
     execute_args = arg_parsor.parse_args()
-
+    #
     # Execute command line
-    if execute_args.p and execute_args.print:
-        print_sip_message_details(execute_args.file)
-    #if execute_args.e and execute_args.exists and execute_args.header:
-        #verify_and_fetch_header_data(execute_args.header, execute_args.file)
+    # if execute_args.p and execute_args.print:
+    #     print_sip_message_details(execute_args.file)
+    # elif execute_args.e and execute_args.exists and execute_args.header:
+    # verify_and_fetch_header_data(execute_args.header, execute_args.file)
 
+    if execute_args.v and execute_args.validate:
+        validate_sip_message(execute_args.file)
 
 
 if __name__ == "__main__":
